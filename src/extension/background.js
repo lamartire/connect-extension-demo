80b049f11a42240014aa95bbc0127b1deec6434b
dudiq <idudiq@gmail.com>
2019-03-11 09:24:03 +0300

    fix: account show, icons, connect version update

--- src/extension/background.js
+++ src/extension/background.js
@@ -2,12 +2,13 @@ chrome.runtime.onInstalled.addListener(function() {
   // add an action here
   console.log('background loaded');
 
-  chrome.browserAction.onClicked.addListener(function(tab) {
-    const contentPayload = {
-      scope: 'endpass',
-      to: 'browser',
-      method: 'openAccount',
-    };
-    chrome.tabs.sendMessage(tab.id, contentPayload);
-  });
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  const contentPayload = {
+    scope: 'endpass',
+    to: 'browser',
+    method: 'openAccount',
+  };
+  chrome.tabs.sendMessage(tab.id, contentPayload);
 });
